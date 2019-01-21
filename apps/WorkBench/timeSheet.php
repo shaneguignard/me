8cdd938963cccc50204e99f20baaa6f132cd8dac
Shane Guignard <shaneguignard@gmail.com>
2019-01-20 22:42:28 -0500

    updated scoresheet files

--- apps/WorkBench/timeSheet.php
+++ apps/WorkBench/timeSheet.php
@@ -6,9 +6,7 @@
     $tk = $_POST["timekeeper"];
     $tkpwd = $_POST["timekeeperpwd"];
     $lightGoals = $_POST["lightGoals"];
-    $lightTeam = $_POST["lightTeam"];
     $darkGoals = $_POST["darkGoals"];
-    $darkTeam = $_POST["darkTeam"];
     $gameDate = $_POST["gameDate"];
     $playedAt = $_POST["playedAt"];
     $arrType = $_POST["type"];
@@ -16,28 +14,16 @@
     $arrPeriod = $_POST["period"];
     $arrTime = $_POST["time"];
     $arrPlayer = $_POST["player"];
+    $arrAssist = $_POST["assist"];
 
     if($tkpwd === '1234'){
-        require_once('remote-mysql.php');
+        require_once('db.php');
     }
     else 
     {
-        echo "Incorrect Password";
+        echo "Incorrect password";
     }
     
-    // Shutout Condition
-        if ($lightGoals == 0){
-            array_push($arrTime, $arrTime[0]);
-            array_push($arrTeam, $lightTeam);
-            array_push($arrType, '');
-            array_push($arrPlayer, '');
-        }
-        if ($darkGoals == 0){
-            array_push($arrTime, $arrTime[0]);
-            array_push($arrTeam, $darkTeam);
-            array_push($arrType, '');
-            array_push($arrPlayer, '');
-        }
     ?>
 <style>
     table {
@@ -51,45 +37,55 @@
     <br>
     <?php
     echo "<h3>$gameDate Summary</h3>";
-    echo "<h4>$lightGoals - $darkGoals</h4>";
+    echo "$lightGaols - $darkGoals"
     ?>
     <table border=1>
         <tr>
             <th>date</th>
+            <th>period</th>
             <th>team</th>
             <th>type</th>
             <th>player</th>
         </tr>
     <?php 
-
-    for($i = 0; $i < count($arrType); $i++){
         
+    //open file
+    $fid = fopen('data/newGame.csv', 'w');
+    $list = array('date', 'period', 'team','type','player');
+    fputcsv($fid, $list);
+    for($i = 0; $i < count($arrType); $i++){
         $gameEvent = $gameDate.' '.$arrTime[$i];
         echo "<tr><td>$gameEvent</td>";
+        echo "<td>$arrPeriod[$i]</td>";
         echo "<td>$arrTeam[$i]</td>";
         echo "<td>$arrType[$i]</td>";
         echo "<td>$arrPlayer[$i]</td></tr>";
-
-//    insert into history.events SQL database
-        $sql = "INSERT INTO `events` VALUES ('$gameEvent', '$arrTeam[$i]', '$arrType[$i]', '$arrPlayer[$i]')";
-
-            $result = $conn->query($sql);
+       
+        $list = array($gameEvent, $arrPeriod[$i], $arrTeam[$i], $arrType[$i], $arrPlayer[$i]);
+        
+//        write backup to newGame.csv
+        fputcsv($fid, $list);
+        
+        
+//    insert into gamehistory SQL server
+        $sql = "INSERT INTO games VALUES ('$gameEvent', '$arrPeriod[$i]', '$arrTeam[$i]', '$arrType[$i]', '$arrPlayer[$i]')";
+        if($conn-> query($sql) != TRUE){
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     }
- 
     $msg = "<html><body><h1>Summary</h1>$gameDate</body></html>";
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    if($result === FALSE){
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-    if ($result === TRUE){
+    if ($conn -> query ($sql) === TRUE){
 	   mail("shaneguignard@gmail.com", "SQL Update", $msg, $headers);
 	   echo "New Records Created";
     }
     
 // close file and connection    
-        $conn->close();
-        ?>
+fclose($fid);
+$conn->close();
+?>
+
     </table>
     
     <a href='scoresheet.html'>New Game</a>
---
+++ apps/remote-mysql.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$host = "35.183.44.111";
+$user = "csnhl";
+$password = "C&snh12019";
+$db = "current";
+
+// Create connection
+$conn = new mysqli($host, $user, $password, $db) or die('Error connecting to server: <br>'. mysqli_connect_error());
+?>
