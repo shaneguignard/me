0903c774ea27809a7f965e664862dc0c7eb63f2c
Shane Guignard <shaneguignard@gmail.com>
2019-05-06 21:59:46 -0400

    updated browser to work for all types

--- apps/WorkBench/bigButton/View/button.php
+++ apps/WorkBench/bigButton/View/button.php
@@ -21,11 +21,10 @@
 </form>
 
 <script>
-var s = navigator.userAgent;
-s = s.split(' ');
-s = s[12].split('/');
+var s = (navigator.userAgent).split(' ').pop();
+s = s.split('/');
 browser = s[0];
-
+console.log(browser);
 var d = document.getElementsByName('device')[0].value = navigator.platform;
 var t = document.getElementsByName('type')[0].value = 'Click';
 var b = document.getElementsByName('browser')[0].value = browser;
--- apps/WorkBench/bigButton/View/content.php
+++ apps/WorkBench/bigButton/View/content.php
@@ -5,6 +5,7 @@
         <th>Type</th>
         <th>IP Address</th>
         <th>Device</th>
+        <th>Browser</th>
         <th>Location</th>
     </tr>
 <?php 
@@ -15,7 +16,7 @@ $result = $conn->query("SELECT * FROM Events");
 if ($result->num_rows > 0){
     while ($row=$result->fetch_assoc())
     {
-        echo '<tr><td>'.$row['timestamp'].'</td><td>'.$row['type'].'</td><td>'.$row['ip'].'</td><td>'.$row['device'].'</td><td>'.$row['location'].'</td></tr>';
+        echo '<tr><td>'.$row['timestamp'].'</td><td>'.$row['type'].'</td><td>'.$row['ip'].'</td><td>'.$row['device'].'</td><td>'.$row['browser'].'</td><td>'.$row['location'].'</td></tr>';
     }
 }
 
